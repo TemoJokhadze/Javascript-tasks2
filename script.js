80a3fc0676ecbd0a648354cdf4d264d6dec250f9
TemoJokhadze <jokh1jokhadze02@gmail.com>
2024-11-27 17:46:00 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// task 1
+
+// let i = 1
+// while (i <= 5) {
+//     console.log(i);
+//     i++ 
+// }
+
+// task 2
+
+// while (true) {
+//     let password = prompt("გთხოვთ, შეიყვანეთ პაროლი:");
+//     if (password === "1234") {
+//         alert("Acces Granted!");
+//         break; 
+//     } else {
+//         alert("Wrong password, try again.");
+//     }
+// }
+
+// task 3
+
+// const arr = ["banana","apple","cherry"]
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// task 4
+
+// let number = 0
+
+// for (let i = 0; i <= 10; i++) {
+//     number += i
+// }
+// console.log(number);
